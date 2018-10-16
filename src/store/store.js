1f939dbd2ece275d065fae1c96e2bef5d8a42ba9
Chandan <chandan7791@gmail.com>
2018-10-16 16:20:37 +0530

    Working on the Portfolio Stocks

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import stocks from './Modules/stocks';
+import portfolio from './Modules/portfolio';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        stocks
+        stocks,
+        portfolio
     }
 });
